c7d58eff14ae849421832a4174a594b8249146d1
gtc <gcheshire@gmail.com>
2026-07-14 08:07:45 -0400

    fix: follow symlinks in custom plugins loader and config picker
    Ports two fixes from upstream kickstart.nvim:
    - f0a2108: pass { follow = true } to vim.fs.dir and accept 'link'
      entries so symlinked plugin files in lua/custom/plugins/ are loaded
    - f660e1f: add follow = true to the <leader>sn find_files call so
      symlinked config files appear in the picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -66,7 +66,7 @@ vim.keymap.set('n', '<leader>s/', function()
   }
 end, { desc = '[S]earch [/] in Open Files' })
 
-vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
+vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config', follow = true } end, { desc = '[S]earch [N]eovim files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Search [G]it Files' })
 vim.keymap.set('n', '<leader>pf', function()
   builtin.grep_string { search = vim.fn.input 'Grep > ' }
